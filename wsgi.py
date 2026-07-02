9a207346b828470537f5a1ccc900a3f6f76958a9
Malick0549 <abdulaiabdulmalick@gmail.com>
2026-07-02 04:06:16 +0000

    fix: Force secure cookies for Railway HTTPS

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@
 import os
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-from backend.flask_app import app as application
+from flask_app import app as application
 app = application
